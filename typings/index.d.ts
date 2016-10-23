a1c064af5a9e25d4da76c21f1556273f459c2326
Trevor Burbidge <Trevor.Burbidge@myresman.com>
2016-10-23 13:58:33 -0600

    Start using npm.

---
+++ typings/index.d.ts
@@ -0,0 +1,5 @@
+/// <reference path="globals/angular/index.d.ts" />
+/// <reference path="globals/bootstrap/index.d.ts" />
+/// <reference path="globals/chance/index.d.ts" />
+/// <reference path="globals/jquery/index.d.ts" />
+/// <reference path="globals/node/index.d.ts" />
