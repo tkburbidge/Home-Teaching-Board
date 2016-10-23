fd04dbf8c6f0374955ff5a3405211aa80e603cae
Trevor Burbidge <Trevor.Burbidge@myresman.com>
2016-10-23 16:40:35 -0600

    Removing all files in .gitignore

--- typings/index.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference path="globals/angular/index.d.ts" />
-/// <reference path="globals/bootstrap/index.d.ts" />
-/// <reference path="globals/chance/index.d.ts" />
-/// <reference path="globals/jquery/index.d.ts" />
-/// <reference path="globals/node/index.d.ts" />
